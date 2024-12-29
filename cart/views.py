ffee1f0af80c60027bcfeb6a40a2acf6b86da788
Mohammad Shuaib <mohammadshuaib108@gmail.com>
2024-12-29 21:43:02 +0530

    created cart app

---
+++ cart/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cart_page(request):
+    return render(request,'cartpage.html')
--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecommerce_app',
+    'cart',
 ]
 
 MIDDLEWARE = [
