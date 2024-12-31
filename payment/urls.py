9eff99936f372bab03b911e346c07d25e5d4c216
Mohammad Shuaib <mohammadshuaib108@gmail.com>
2024-12-31 15:49:03 +0530

    checkout page

--- payment/urls.py
+++ payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('payment_success/',views.payment_success,name = 'payment_success'),
     path('editshipping/',views.edit_shipping_info,name = 'edit_shipping'),
+    path('checkout/', views.checkout,name = 'checkout'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from cart.cart import Cart
 from django.contrib.auth.decorators import login_required
 from .forms import ShippingAddressForm
 from .models import ShippingAddress
 # Create your views here.
+
+
 def payment_success(request):
+    '''demo one.'''
     return render(request,"payment_success.html")
 
 @login_required
@@ -31,3 +35,24 @@ def edit_shipping_info(request):
     else:
         form = ShippingAddressForm(instance = shipping_user)
     return render(request,'edit_shipping.html',{'form':form})
+
+@login_required
+def checkout(request):
+    cart = Cart(request)
+    cart_items = cart.get_items()
+    total_price = cart.get_total_price()
+
+    try:
+        shipping_address = ShippingAddress.objects.get(user = request.user)
+    except ShippingAddress.DoesNotExist:
+        shipping_address = None
+        return redirect('edit_shipping')
+    
+    if request.method == 'POST':
+        #integrate payment module
+        return redirect('payment_success')
+    
+    return render(request,'checkout.html',{
+        'cart_items': cart_items,
+        'total_price':total_price,
+        'shipping_address': shipping_address})
