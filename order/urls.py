61891a2510ec7b33169958621dcd094fbf1e1724
Mohammad Shuaib <mohammadshuaib108@gmail.com>
2024-12-31 17:03:53 +0530

    order cancel feature added

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('order_status/',views.order_status, name = 'order_status'),
+    path('cancel_order<int:order_id>/',views.cancel_order,name = 'cancel_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Order
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -7,3 +8,13 @@ from django.contrib.auth.decorators import login_required
 def order_status(request):
     orders = Order.objects.filter(customer = request.user.customer).order_by('-order_date')
     return render(request,'order_status.html',{'orders': orders})
+
+def cancel_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id, customer = request.user.customer)
+    if order.status == 'Pending':
+        order.status = 'Cancelled'
+        order.save()
+        messages.success(request, "Your order has been cancelled Successfully!")
+    else:
+        messages.error(request, "You can only cancel pending orders.")
+    return redirect('order_status')
